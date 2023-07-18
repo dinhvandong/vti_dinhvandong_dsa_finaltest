99a40aa231afc4c28f2d698cfed23bcce537588b
LAPTOP-T4IEN2K1\TUF_Gaming <dongdinhvan.edu@gmail.com>
2023-07-18 16:20:36 +0700

    Final project

--- FinalTest/src/main/java/org/example/Main.java
+++ FinalTest/src/main/java/org/example/Main.java
@@ -3,5 +3,23 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        char ch1 = '5';
+        char ch2 = '@';
+
+        boolean result1 = Character.isLetter(ch1);  // Returns false
+        boolean result2 = Character.isLetter(ch2);  // Returns true
+
+        System.out.println(ch1 + " is a letter: " + result1);
+        System.out.println(ch2 + " is a letter: " + result2);
+    }
+
+   static  boolean isNormal(char c){
+        // 0-9
+       // Aa Bb Cc Ww.
+       if(Character.isDigit(c) || Character.isLetter(c)){
+           return true;
+       }
+        return false;
     }
 }
